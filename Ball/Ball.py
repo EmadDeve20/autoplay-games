fb0b35cfd93b07cdf44f68dc51a2f5a8b82e865d
ED <emaddeve20@gmail.com>
2021-06-07 22:37:46 +0430

    clean code !

--- Ball/Ball.py
+++ Ball/Ball.py
@@ -1,139 +1,135 @@
 #!/bin/env python3
 
-import pygame , random
+import pygame, random
 
 class Room:
-    def __init__(self):
+    def __init__(self, width: int=500, height: int=500, speed=60, ):
         """
         init room
         """
         pygame.init()
-
         #the first step creating a form
-        self.width = 500
-        self.height = 500
+        self.width = width
+        self.height = height
         self.form = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Ball")
-
         #second, we need logic and a playground(play space)
-        self.speed = 60
+        self.speed = speed
         self.clock = pygame.time.Clock()
         self.background = (250, 250, 250)
-
-    def close(self):
-        """
-            from need close for user
-        """
-        pygame.quit()
-        quit()
-
-    def ball(self, color, x = 100 , y = 100 , w = 0):
-        """this function can created a ball"""
-        bull =  pygame.draw.circle(self.form , color , (x , y) , 50 ,w)
-        self.form.blit(self.form , bull)
-
-    def run(self):
         # We need a random coordinate to make the ball in the first run
-        x = random.randrange(50,200)
-        y = random.randrange(50,200)
-
+        self.x_location = random.randrange(50, 200)
+        self.y_location = random.randrange(50, 200)
         # first, run the ball moving up or down? this is random (up or down)
-        i = random.randrange(2)
-        if i == 0:
-            y_change = -1
-        else:
-            y_change = 1
-
+        self.y_changer = self.up_or_down()
         # first run , ball moving left or right? this is random (left or right)
-        i = random.randrange(2)
-        if i == 0:
-            x_change = 1
-        else:
-            x_change = -1
-
-        # This ball can be faster or slower. 
+        self.x_changer = self.left_or_right()
+        # This ball can be faster or slower.
         # But this is a coincidence and remains the same until the end.
         # That is, it either gets faster or slower!
-        i = random.randrange(2)
-        if i == 0:
-            speed_change = -1
-        else:
-            speed_change = 1
+        self.speed_changer = self.faster_or_slower()
+        self.width_ball = 0
+        self.width_ball_changer = 1
 
-        w = 0 ; w_change = 1
-        color = (random.randrange(256) , random.randrange(256) , random.randrange(256))
-        exiting = False
+    def ball(self, color, x_location=100, y_location=100, width=0):
+        """this function can created a ball"""
+        bull = pygame.draw.circle(self.form, color, (x_location, y_location), 50, width)
+        self.form.blit(self.form, bull)
+
+    def up_or_down(self):
+        """return up(-1) or down(1)"""
+        if random.randrange(2) == 1:
+            return 1
+        return -1
+
+    def left_or_right(self):
+        """return left(-1) or right(1)"""
+        if random.randrange(2) == 1:
+            return 1
+        return -1
+
+    def faster_or_slower(self):
+        """return faster(1) or lower(-1)"""
+        if random.randrange(2) == 1:
+            return 1
+        return -1
+
+    def run(self):
+        """RUN!"""
+        color = (random.randrange(256), random.randrange(256), random.randrange(256))
+        exit = False
 
         # This is the logic of the game
-        while not exiting:
+        while not exit:
             for eve in pygame.event.get():
                 if eve.type == pygame.QUIT:
-                    self.close()
+                    exit = True
                 if eve.type == pygame.KEYDOWN:
                     if eve.key == pygame.K_ESCAPE or eve.key == pygame.K_q:
-                        self.close()
+                        exit = True
 
             # Changes take shape here
             self.form.fill(self.background)
-            self.ball(color , x , y , w)
-            y +=y_change
-            x +=x_change
-            w += w_change
+            self.ball(color, self.x_location, self.y_location, self.width_ball)
+            self.y_location += self.y_changer
+            self.x_location += self.x_changer
+            self.width_ball += self.width_ball_changer
 
-            #print(w)
-            if w == 50:
-                w_change = -1
-            if w == 0:
-                w_change = 1
+            if self.width_ball == 50:
+                self.width_ball_changer = -1
+            if self.width_ball == 0:
+                self.width_ball_changer = 1
 
             # Now the ball has hit the top wall
-            if y-55 < 0 and y_change == -1:
+            if self.y_location-55 < 0 and self.y_changer == -1:
                 # the ball need moving down
-                y_change = 1
+                self.y_changer = 1
                 # If the ball hits the wall, its speed changes
                 # If the speed of the ball is fast, it will be faster
                 # If the ball is slow, it will be slower
-                if (speed_change == 1 and self.speed < 1000) or \
-                    (speed_change == -1 and self.speed > 2):
-                    self.speed += speed_change
+                if (self.speed_changer == 1 and self.speed < 1000) or \
+                    (self.speed_changer == -1 and self.speed > 2):
+                    self.speed += self.speed_changer
                 # If the ball hits the wall, its color can change
-                color = (random.randrange(256) , random.randrange(256) , random.randrange(256))
-            
+                color = (random.randrange(256), random.randrange(256), random.randrange(256))
+
             # Now the ball has hit the down wall
-            if y+55 > self.height - 200 and y_change == 1:
+            if self.y_location+55 > self.height - 200 and self.y_changer == 1:
                 # the ball need moving up
-                y_change = -1
+                self.y_changer = -1
                 # If the ball hits the wall, its speed changes
                 # If the speed of the ball is fast, it will be faster
                 # If the ball is slow, it will be slower
-                if (speed_change == 1 and self.speed < 1000) or \
-                    (speed_change == -1 and self.speed > 2):
-                    self.speed += speed_change
+                if (self.speed_changer == 1 and self.speed < 1000) or \
+                    (self.speed_changer == -1 and self.speed > 2):
+                    self.speed += self.speed_changer
                 # If the ball hits the wall, its color can change
-                color = (random.randrange(256) , random.randrange(256) , random.randrange(256))
-            
+                color = (random.randrange(256), random.randrange(256), random.randrange(256))
+
             # Now the ball has hit the right wall
-            if x+55 > self.width - 200 and x_change == 1:
+            if self.x_location+55 > self.width - 200 and self.x_changer == 1:
                 # ball need moving left
-                x_change = -1
+                self.x_changer = -1
                 # If the ball hits the wall, its speed changes
                 # If the speed of the ball is fast, it will be faster
                 # If the ball is slow, it will be slower
-                if (speed_change == 1 and self.speed < 1000) or (speed_change == -1 and self.speed > 2):
-                    self.speed += speed_change
+                if (self.speed_changer == 1 and self.speed < 1000) or \
+                (self.speed_changer == -1 and self.speed > 2):
+                    self.speed += self.speed_changer
                 # If the ball hits the wall, its color can change
-                color = (random.randrange(256) , random.randrange(256) , random.randrange(256))
+                color = (random.randrange(256), random.randrange(256), random.randrange(256))
 
             # Now the ball has hit the left wall
-            if x - 55 < 0 and x_change == -1:
-                x_change = 1
+            if self.x_location - 55 < 0 and self.x_changer == -1:
+                self.x_changer = 1
                 # If the ball hits the wall, its speed changes
                 # If the speed of the ball is fast, it will be faster
                 # If the ball is slow, it will be slower
-                if (speed_change == 1 and self.speed < 1000) or (speed_change == -1 and self.speed > 2):
-                    self.speed += speed_change
+                if (self.speed_changer == 1 and self.speed < 1000) or \
+                (self.speed_changer == -1 and self.speed > 2):
+                    self.speed += self.speed_changer
                 # If the ball hits the wall, its color can change
-                color = (random.randrange(256) , random.randrange(256) , random.randrange(256))
+                color = (random.randrange(256), random.randrange(256), random.randrange(256))
 
             # This is the passage of time
             self.clock.tick(self.speed)
